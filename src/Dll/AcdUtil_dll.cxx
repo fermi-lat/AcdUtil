bd66340b67df4b4dfae7a9789740f8de4e818b6d
echarles <>
2006-04-17 19:05:55 +0000

    Work some obscure magic so that windows understands what to do

--- src/Dll/AcdUtil_dll.cxx
+++ src/Dll/AcdUtil_dll.cxx
@@ -1,7 +1,8 @@
 /** @file AcdUtil_dll.cxx
 
-  $Header$
+  $Header: /nfs/slac/g/glast/ground/cvs/AcdUtil/src/Dll/AcdUtil_dll.cxx,v 1.1.1.1 2005/11/08 17:28:16 heather Exp $
 */
 
 #include "GaudiKernel/LoadFactoryEntries.h"
 LOAD_FACTORY_ENTRIES(AcdUtil)
+
--- src/Dll/AcdUtil_load.cxx
+++ src/Dll/AcdUtil_load.cxx
@@ -3,13 +3,14 @@
 * @brief This is needed for forcing the linker to load all components
 * of the library.
 *
-*  $Header$
+*  $Header: /nfs/slac/g/glast/ground/cvs/AcdUtil/src/Dll/AcdUtil_load.cxx,v 1.1.1.1 2005/11/08 17:28:16 heather Exp $
 */
 
 #include "GaudiKernel/DeclareFactoryEntries.h"
 
 DECLARE_FACTORY_ENTRIES(AcdUtil) {
     DECLARE_SERVICE( AcdGeometrySvc );
+    DECLARE_SERVICE( AcdCalibSvc );
     DECLARE_ALGORITHM( TestAcdUtil );
 } 
 
