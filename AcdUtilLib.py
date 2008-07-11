e90e9d7e4cfa380df925e5e7ca1b9c31a0445a0a
glast <>
2008-07-11 00:32:26 +0000

    Changes to allow GR packages to build on windows

--- AcdUtilLib.py
+++ AcdUtilLib.py
@@ -1,7 +1,8 @@
-# $Header$
+# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AcdUtil/AcdUtilLib.py,v 1.1 2008/07/09 21:13:40 glastrm Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = ['AcdUtil', 'AcdUtilCommon'])
+        env.Tool('addLibrary', library = ['AcdUtil'])
+    env.Tool('addLibrary', library = ['AcdUtilCommon'])
     env.Tool('CalibDataLib')
     env.Tool('geometryLib')
     env.Tool('xmlBaseLib')
