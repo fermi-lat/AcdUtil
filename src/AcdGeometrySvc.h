b216f21ef5f9cb73eb2df0b56538a9634ec6d570
echarles <>
2009-06-08 17:18:06 +0000

    updated signature of AcdGeometrySvc::getRibbon() and added AcdGeometrySvc::getNextTileCorners() to handle gaps

--- src/AcdGeometrySvc.h
+++ src/AcdGeometrySvc.h
@@ -12,7 +12,7 @@
  * 
  * @author Heather Kelly 
  *
- * $Header: /nfs/slac/g/glast/ground/cvs/AcdUtil/src/AcdGeometrySvc.h,v 1.16 2008/01/23 23:42:18 echarles Exp $
+ * $Header: /nfs/slac/g/glast/ground/cvs/AcdUtil/src/AcdGeometrySvc.h,v 1.17 2008/05/24 00:31:01 echarles Exp $
  */
 
 #include "GaudiKernel/Service.h"
@@ -80,12 +80,8 @@ public:
 
     /// Given an AcdId, provide three vectors of Rays.  Each vector pertains to one set of ribbon segments
     bool fillRibbonData(const idents::AcdId& id,
-			std::vector<Ray>& minusSideRays,
-			std::vector<Ray>& topRays,
-			std::vector<Ray>& plusSideRays, 
-			HepTransform3D& minusSideTransform,
-			HepTransform3D& topTransform,
-			HepTransform3D& plusTransform);
+			std::vector<AcdRibbonSegment*>& segs,
+			int& topIdx, int& plusIdx);
     
     /// Return half ribbon width
     virtual double ribbonHalfWidth() const;
@@ -110,6 +106,10 @@ public:
     AcdFrameUtil::AcdReferenceFrame getReferenceFrame(const idents::VolumeIdentifier &volId) const;
 
     StatusCode findCornerGaps();
+
+    virtual StatusCode getNextTileCorners(const idents::AcdId& id, int dir, 
+					  HepPoint3D& c1, HepPoint3D& c1, bool& isRealGap);
+
 
 protected:
 
