9d5f8b2b21f1d5faaa7b4ef83a45cdf1b2757472
heather <>
2011-08-01 19:11:13 +0000

    merge in SCons updates for P7

--- AcdUtilLib.py
+++ AcdUtilLib.py
@@ -1,4 +1,4 @@
-# $Header: /nfs/slac/g/glast/ground/cvs/GlastRelease-scons/AcdUtil/AcdUtilLib.py,v 1.2 2008/07/11 00:32:26 glast Exp $
+# $Header: /nfs/slac/g/glast/ground/cvs/AcdUtil/AcdUtilLib.py,v 1.4 2009/08/08 01:08:04 jrb Exp $
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['AcdUtil'])
@@ -6,7 +6,17 @@ def generate(env, **kw):
     env.Tool('CalibDataLib')
     env.Tool('geometryLib')
     env.Tool('xmlBaseLib')
+    env.Tool('rdbModelLib')
+    env.Tool('calibUtilLib')
     env.Tool('EventLib')
     env.Tool('GlastSvcLib')
+    env.Tool('mootCoreLib')
+    env.Tool('facilitiesLib')
+    env.Tool('addLibrary', library = env['gaudiLibs'])
+    env.Tool('addLibrary', library = env['mysqlLibs'])
+    env.Tool('addLibrary', library = env['clhepLibs'])
+    env.Tool('addLibrary', library = env['cppunitLibs'])
+    env.Tool('addLibrary', library = env['xercesLibs'])
+
 def exists(env):
     return 1;
